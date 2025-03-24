9cf3d4abcc5c72ccf8d3f738f6cd0fa272da5152
angelikaMn <angelikamanoppo02@gmail.com>
2025-03-24 14:06:57 +0800

    exercise7

--- RestAPI.jsx
+++ RestAPI.jsx
@@ -1,27 +1,42 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {View, Text, FlatList, StyleSheet} from 'react-native';
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import UserItem from './UserItem';
 
 const RestAPI = () => {
   const [users, setUsers] = useState([]);
+
   useEffect(() => {
     axios
-      .get('https://jsonplaceholder.typicode.com/users')
-      .then(res => setUsers(res.data));
+      .get('https://reqres.in/api/users?per_page=12&authuser=0')
+      .then(res => setUsers(res.data.data))
+      .catch(err => console.error(err));
   }, []);
 
   return (
-    <View>
-      <Text>RestAPI</Text>
-      {users.map(item => {
-        return (
-          <Text key={item.id} style={{fontSize: 20}}>
-            {item.name}
-          </Text>
-        );
-      })}
+    <View style={styles.container}>
+      <Text style={styles.title}>User List</Text>
+      <FlatList
+        data={users}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => <UserItem user={item} />}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    paddingTop: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 30,
+    marginBottom: 15,
+  },
+});
 
 export default RestAPI;
